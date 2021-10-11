96b22ffe20162c4aa78d95dad3b3e00809ea6d49
Nadim Nawshad <60882521+nadimnawshad@users.noreply.github.com>
2021-10-11 12:44:27 +0600

    Update result.php

--- Task2_20426091/result.php
+++ Task2_20426091/result.php
@@ -7,7 +7,7 @@ $validatecheckbox="";
 $validateradio="";
 $j=$p=$c="";
 
-$fname=$lname=$password=$email=$age=$designation=$language=$filetoupload="";
+$fname=$lname=$password=$email=$age=$designation=$filetoupload="";
 if($_SERVER["REQUEST_METHOD"]=="POST")
 {
 $fname=$_REQUEST["fname"];
@@ -16,7 +16,6 @@ $email=$_REQUEST["email"];
 $password=$_REQUEST["password"];
 $age=$_REQUEST["age"];
 $filetoupload =$_REQUEST["filetoupload"];
-$language=$_REQUEST["language"];
 
 if(empty($_REQUEST["fname"]) || (strlen($_REQUEST["fname"])<5))
 {
@@ -32,9 +31,9 @@ if(empty($_REQUEST["lname"]) || (strlen($_REQUEST["lname"])<5))
 }
 
 
-if(empty($_REQUEST["age"]) || is_numeric($_REQUEST["age"]))
+if(empty($_REQUEST["age"]) || !is_numeric($_REQUEST["age"]))
 {
-    $validatelname= "Please enter your age";
+    $validateage= "Please enter your age";
 
 }
 
@@ -43,6 +42,11 @@ if(empty($email) || !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\
     $validateemail="Please enter email";
 }
 
+if(empty($_REQUEST["password"]))
+{
+    $validateage= "Please enter your password";
+
+}
 
 if(!isset($_REQUEST["JAVA"])&&!isset($_REQUEST["PHP"])&&!isset($_REQUEST["C++"]))
 {
@@ -77,7 +81,7 @@ if(!isset($_REQUEST["designation"]))
           'lastName'=> $lname,
           'age'=> $age,
           'designation'=>$designation,
-          'language'=>$language,
+          'language'=>array($j,$p,$c),
           'email'=>$email,
           'password'=>$password,
           'image'=>$filetoupload
